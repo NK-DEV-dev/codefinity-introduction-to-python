3879f40b34271c241e72afb128d5c608bb7c79f5
NK-DEV-dev <>
2026-01-21 02:32:45 +0000

    Membership and Types

---
+++ conditional_statements/membership_and_types/main.py
@@ -0,0 +1,9 @@
+# Variable t
+t = "Introduction to Python"
+# Complete the first part of task
+print(len(t) < 15 and t.count('n') > 2)
+
+# Variable a
+a = 89
+# Complete the second part of task
+print(a > 9475 / 37 or a % 14 < 7)
